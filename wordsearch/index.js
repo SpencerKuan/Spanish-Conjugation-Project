80eb594f53ab5c4cfb7e9a4fe67ce21b37d958bf
SpencerKuan <spencer.kuan.co@gmail.com>
2023-01-30 15:02:51 -0900

    dual project

---
+++ wordsearch/index.js
@@ -0,0 +1,228 @@
+
+let reflexive;
+let pick = list => list[Math.floor(Math.random()*list.length)];
+
+
+class wordGrid {
+    constructor(generator, size = 10, cellSize = 600 / size) {
+        this.rows = size;
+        this.cols = size;
+        this.grid = [];
+        this.cellSize = cellSize;
+
+        this.createGrid(generator);
+    }
+
+    createGrid(generator = () => " "){
+        this.grid = [];
+        for(let i = 0; i < this.rows; i++){
+            for(let j = 0; j < this.cols; j++){
+                this.grid.push(generator(i, j));
+            }
+        }
+    }
+
+    write(word, x, y, dx, dy, override = false){
+        let isSameOrEmpty = true;
+        for(let i = 0; i < word.length; i++) {
+            const letter = this.grid[(y + i * dy) * this.cols + x + i * dx];
+            isSameOrEmpty = isSameOrEmpty && (letter === " " || letter === word[i]);
+        }
+
+        if (
+            (x + word.length * dx > this.cols || x + word.length * dx < 0) || 
+            (y + word.length * dy > this.rows || y + word.length * dy < 0) || 
+            !isSameOrEmpty && !override
+        ) return false;
+
+        for(let i = 0; i < word.length; i++){
+            this.grid[(y + i * dy) * this.cols + x + i * dx] = word[i];
+        }
+
+        return true;
+    }
+
+    writeHorizontal = (word, x, y, override = false) => this.write(word, x, y, 1, 0, override)
+    writeVertical = (word, x, y, override = false) => this.write(word, x, y, 0, 1, override)
+    writeDiagonal = (word, x, y, override = false) => this.write(word, x, y, 1, 1, override)
+
+    findSpace(word, maxIterations = 10){
+        let x, y, dx, dy;
+        let found = false;
+        let i = 0;
+
+        while(!found){
+            x = Math.floor(Math.random() * this.cols);
+            y = Math.floor(Math.random() * this.rows);
+            dx = Math.floor(Math.random() * 2);
+            dy = Math.floor(Math.random() * 2);
+
+            if (dx === 0 && dy === 0) continue;
+
+            found = this.write(word, x, y, dx, dy);
+            if (++i > maxIterations) return false;
+        }
+
+        return {x, y, dx, dy};
+    }
+
+    draw(){
+        textSize(32);
+        textAlign(CENTER, CENTER);
+
+        stroke(255);
+        for(let i = 0; i < this.rows; i++){
+            let y = i * this.cellSize;
+            line(0, y, this.cols * this.cellSize, y);
+        }
+        for(let i = 0; i < this.cols; i++){
+            let x = i * this.cellSize;
+            line(x, 0, x, this.rows * this.cellSize);
+        }
+
+        for(let i = 0; i < this.rows; i++){
+            for(let j = 0; j < this.cols; j++){
+                let x = j * this.cellSize;
+                let y = i * this.cellSize;
+
+                fill(255);
+                text(this.grid[i * this.cols + j], x + this.cellSize/2, y + this.cellSize/2);
+            }
+        }
+    }
+
+    static randomLetter(){
+        return String.fromCharCode(97 + Math.floor(Math.random() * 26));
+    }
+};
+
+// word grid
+const grid = new wordGrid();
+
+// words to add
+var words = [];
+
+// used words
+let wordsAdded = [];
+
+// all tasks to run async in the draw loop
+const tasks = new Set();
+
+function generate(){
+    words = reflexive.reflexiveWords.map(x => {
+        const pronoun = pick(reflexive.pronouns);
+        const word = reflexive.conjugateReflexive(x, pronoun).replaceAll(" ", "");
+
+        return {
+            word: word,
+            hint: pronoun + " + " + x.word
+        }
+    });
+    wordsAdded = [];
+
+    grid.createGrid();
+
+    const propogate = createSpreadTask(propogateWords);
+    propogate.then(x => {
+        console.log("done", x);
+
+        renderWordList(x);
+
+        // replace each empty square with a random letter
+        grid.grid = grid.grid.map(x => x === " " ? pick("abcdefghijklmnopqrstuvwxyzáéíñ".split('')) : x);
+    })
+}
+
+let canvas;
+async function setup(){
+    noStroke();
+
+    canvas = createCanvas(600, 600);
+    canvas.canvas.classList.add("output-canvas");
+
+    await loadDependencies();
+    generate();
+}
+
+
+let propogateWords = (() => {    
+    let iterations = 0;
+
+    return function propogateWords(){
+        for(var i = 0; i < 100; i ++){
+            if (words.length){
+                let wordIndex = Math.floor(Math.random() * words.length);
+                let word = words[wordIndex];
+                let found = !!grid.findSpace(word.word, 10);
+            
+                if (found) {
+                    wordsAdded.push(word);
+                    words.splice(wordIndex, 1);
+                }
+            }
+        }
+    
+        if(!words.length || ++iterations >= 60) return wordsAdded;
+    }
+})();
+
+
+tasks.add(function render(){
+    background(0);
+    grid.draw();
+})
+
+function draw (){
+    for(let task of tasks){
+        task(task);
+    }
+}
+
+
+// --- utils --- //
+
+function randomVerb(){
+    var pronoun = pick(reflexive.pronouns);
+    var word = pick(reflexive.reflexiveWords);
+    
+    return reflexive.conjugateReflexive(word, pronoun);
+}
+
+function renderWordList(words){
+    let element = document.getElementById("wordListItems");
+    element.innerHTML = "";
+
+    words.forEach(word => {
+        let li = document.createElement("li");
+        li.innerHTML = word.hint;
+        element.appendChild(li);
+    });
+}
+
+// load dependencies
+async function loadDependencies(){
+    reflexive = await import ("./reflexive.js");
+    console.log(reflexive.pronouns);
+}
+
+function createSpreadTask(work, maxIts = null){
+    let iterations = 0;
+
+    const promise = new Promise((resolve, reject) => {
+        const task = function(){
+            const done = work();
+
+            if (done) {
+                tasks.delete(task);
+                resolve(done);
+            } else if (maxIts && ++iterations > maxIts){
+                tasks.delete(task);
+                reject("Max iterations reached");
+            }
+        }
+
+        tasks.add(task);
+    });
+    
+    return promise;
+}
---
+++ wordsearch/reflexive.js
@@ -0,0 +1,147 @@
+const pronouns = ["yo", "tú", "él", "ella", "usted", "nosotros", "vosotros", "ellos", "ustedes"];
+const pronounMap = {
+    "yo": {
+        reflexive: "me",
+        boot: true
+    },
+    "tú": {
+        reflexive: "te",
+        boot: true
+    },
+    "él": {
+        reflexive: "se",
+        boot: true
+    },
+    "ella": {
+        reflexive: "se",
+        boot: true
+    },
+    "usted": {
+        reflexive: "se",
+        boot: true
+    },
+    "nosotros": {
+        reflexive: "nos",
+        boot: false
+    },
+    "vosotros": {
+        reflexive: "os",
+        boot: false
+    },
+    "ellos": {
+        reflexive: "se",
+        boot: true
+    },
+    "ustedes": {
+        reflexive: "se",
+        boot: true
+    }
+}
+
+const endingTable = {
+    "ar": {
+        "yo": "o",
+        "tú": "as",
+        "él": "a",
+        "ella": "a",
+        "usted": "a",
+        "nosotros": "amos",
+        "vosotros": "áis",
+        "ellos": "an",
+        "ustedes": "an"
+    },
+    "er": {
+        "yo": "o",
+        "tú": "es",
+        "él": "e",
+        "ella": "e",
+        "usted": "e",
+        "nosotros": "emos",
+        "vosotros": "éis",
+        "ellos": "en",
+        "ustedes": "en"
+    },
+    "ir": {
+        "yo": "o",
+        "tú": "es",
+        "él": "e",
+        "ella": "e",
+        "usted": "e",
+        "nosotros": "imos",
+        "vosotros": "ís",
+        "ellos": "en",
+        "ustedes": "en"
+    },
+}
+
+const reflexiveList = (`
+    acostarse (o-ue) to go to bed
+    afeitarse to shave
+    arreglarse to get ready
+    bañarse to take a bath, to bathe
+    cansarse (de) to get tired (of)
+    casarse (con) to get married (to)
+    cortarse to cut onself, to get cut (hair)
+    despedirse (e-i) to say goodbye to
+    despertarse (e-ie)to wake up
+    ducharse to take a shower
+    enfermarse to get sick
+    lastimarse to hurt oneself
+    lavarse to wash
+    llamarse to call oneself
+    levantarse to get up
+    peinarse to comb one's hair
+    ponerse to get dressed
+    quedarse to stay
+    quitarse to take off (clothing)
+    secarse to dry
+    sentarse (e-ie) to sit down
+    sentirse (e-ie) to feel
+`);
+
+let reflexiveWords = reflexiveList.trim().split('\n').map(x => {
+    let res = x.trim().split(" ");
+    res[1] = res[1].split('-').length > 1 ? res[1].slice(1, -1).split('-') : false;
+
+    return {
+        word: res[0],
+        irregular: res[1].length ? res[1] : false
+    }
+
+    return res;
+});
+
+function conjugateReflexive(reflexiveVerbObject, pronoun){
+    // get the pronoun prefix (me/te/se) object
+    const pronounPrefix = pronounMap[pronoun];
+
+    let reflexiveVerb = reflexiveVerbObject.word.trim();
+    let irregular = reflexiveVerbObject.irregular;
+
+    // replace appropriate letters in irregular verb
+    reflexiveVerb = irregular &&  pronounPrefix.boot? 
+        reflexiveVerb.replace(irregular[0], irregular[1]) : reflexiveVerb;
+
+    // find the base infinitive verb (non-reflexive like "hablar"), remove the "se"
+    const baseVerbInfinitive = reflexiveVerb.slice(0, -2);
+
+    // remove the "ar/er/ir"
+    const baseVerb = baseVerbInfinitive.slice(0, -2);
+
+    // get the verb type (ar/er/ir)
+    const verbType = baseVerbInfinitive.slice(-2);
+
+    // get the conjugated ending for the verb
+    const ending = endingTable[verbType][pronoun];
+
+    if (pronounPrefix.reflexive && baseVerb && ending){
+        // return the conjugated verb with pronoun "me lavo"
+        return (pronounPrefix.reflexive) + " " + (baseVerb + ending);
+    } else {
+        throw new Error("Could not conjugate verb");
+    }
+}
+
+export {
+    pronouns, reflexiveWords, conjugateReflexive
+}
